b78828836eee7c4043de84291a7c3db16469c369
majorgrizzly <sid@parekh.co.in>
2020-05-12 22:25:07 +0530

    Create classify.py

---
+++ classify.py
@@ -0,0 +1,44 @@
+
+from tensorflow.keras.models import load_model
+import cv2
+import numpy as np
+import argparse
+
+def preprocess(path):
+    image = cv2.imread(path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = cv2.resize(image, (224, 224))
+    image = image/255.0
+    return image
+
+def change(i):
+    return (1-i)
+
+def output(i):
+    i *= 100
+    if i<50:
+        print("NEGATIVE")
+        print("Confidence Score - "+str(100-i)+"%")
+    else:
+        print("POSITIVE")
+        print("Confidence Score - "+str(i)+"%")
+
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--imagepath", default="\image.jpg",
+	help="path to input image of x-ray")
+ap.add_argument("-m", "--modelpath", default="\model.h5")
+args = vars(ap.parse_args())
+
+imagepath = args["imagepath"]
+modelpath = args["modelpath"]
+
+model = load_model(modelpath)
+image = preprocess(imagepath)
+
+data = []
+data.append(image)
+data = np.array(data)
+
+predict = change(model.predict(data))
+output(predict[0][0])
